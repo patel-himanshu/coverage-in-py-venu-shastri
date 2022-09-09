536b5c1ecee8e8a87f82db4b5318d698a2d25f00
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 14:53:04 +0530

    Fixed test case

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -7,11 +7,14 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
 
   def test_infers_breach_as_per_limits_too_high(self):
-    self.assertTrue(typewise_alert.infer_breach(10, 50, 100) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(120, 50, 100) == 'TOO_HIGH')
 
   def test_infers_breach_as_per_limits_normal(self):
     self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
 
+  def test_function_calls(self):
+    typewise_alert.send_to_controller("TOO_HIGH")
+    typewise_alert.send_to_email("TOO_LOW")
 
 if __name__ == '__main__':
   unittest.main()
