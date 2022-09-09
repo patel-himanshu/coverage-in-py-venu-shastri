467741677c3ea1509f9f59da008e9055fd1d61e2
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 14:37:06 +0530

    Fixed equality check operator

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -7,10 +7,10 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
 
   def test_infers_breach_as_per_limits_too_high(self):
-    self.assertTrue(typewise_alert.infer_breach(10, 50, 100) === 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(10, 50, 100) == 'TOO_HIGH')
 
   def test_infers_breach_as_per_limits_normal(self):
-    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) === 'NORMAL')
+    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
 
 
 if __name__ == '__main__':
