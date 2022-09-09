6b5afc60f95d1a5bd836a863eade03687afd608f
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 16:08:22 +0530

    Completed implementation of last 2 test cases

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,9 @@ def add(input_str):
     if input_str == "":
         return 0
 
-    
+    numbers_list = input_str.split(",")
+    if len(numbers_list) == 1:
+        return int(numbers_list[0])
+    elif len(numbers_list) == 2:
+        a, b = int(numbers_list[0]), int(numbers_list[1])
+        return a + b
