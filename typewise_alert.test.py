3899a47fa1effbba5e794aa4c7f958fbc595549b
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 14:36:11 +0530

    Added 2 test cases for infer_breach

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -3,8 +3,14 @@ import typewise_alert
 
 
 class TypewiseTest(unittest.TestCase):
-  def test_infers_breach_as_per_limits(self):
+  def test_infers_breach_as_per_limits_too_low(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+
+  def test_infers_breach_as_per_limits_too_high(self):
+    self.assertTrue(typewise_alert.infer_breach(10, 50, 100) === 'TOO_HIGH')
+
+  def test_infers_breach_as_per_limits_normal(self):
+    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) === 'NORMAL')
 
 
 if __name__ == '__main__':
