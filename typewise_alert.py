497d8c27896cb2ead59989129f7e95d5df8ecd17
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 14:17:44 +0530

    Changed text message

--- typewise_alert.py
+++ typewise_alert.py
@@ -40,7 +40,7 @@ def send_to_email(breachType):
   recepient = "a.b@c.com"
   if breachType == 'TOO_LOW':
     print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
+    print('Hello, the temperature is too low')
   elif breachType == 'TOO_HIGH':
     print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+    print('Hello, the temperature is too high')
