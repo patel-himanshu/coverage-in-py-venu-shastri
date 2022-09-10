1918d1d8f10116c84ddb5a00dd35caaa09dcaa44
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-11 01:44:12 +0530

    Updated test case for medium active cooling

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -15,11 +15,11 @@ class TypewiseTest(unittest.TestCase):
 	def test_classify_temperaturee_breach_passive_cooling(self):
 		self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 10) == 'NORMAL')
 
-	def test_classify_temperaturee_breach_med_active_cooling(self):
-		self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 10) == 'NORMAL')
-
 	def test_classify_temperaturee_breach_hi_active_cooling(self):
 		self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 50) == 'TOO_HIGH')
+
+	def test_classify_temperaturee_breach_med_active_cooling(self):
+		self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', -10) == 'TOO_LOW')
 
 	def test_function_calls(self):
 		typewise_alert.send_to_controller("TOO_HIGH")
