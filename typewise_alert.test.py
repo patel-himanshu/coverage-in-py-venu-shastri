75e89aa0441d697343f3b7c95bb0cb370d5c4cac
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-11 01:39:37 +0530

    Added 3 test cases for typewise_alert

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -3,18 +3,27 @@ import typewise_alert
 
 
 class TypewiseTest(unittest.TestCase):
-  def test_infers_breach_as_per_limits_too_low(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+	def test_infers_breach_as_per_limits_too_low(self):
+		self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
 
-  def test_infers_breach_as_per_limits_too_high(self):
-    self.assertTrue(typewise_alert.infer_breach(120, 50, 100) == 'TOO_HIGH')
+	def test_infers_breach_as_per_limits_too_high(self):
+		self.assertTrue(typewise_alert.infer_breach(120, 50, 100) == 'TOO_HIGH')
 
-  def test_infers_breach_as_per_limits_normal(self):
-    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
+	def test_infers_breach_as_per_limits_normal(self):
+		self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
 
-  def test_function_calls(self):
-    typewise_alert.send_to_controller("TOO_HIGH")
-    typewise_alert.send_to_email("TOO_LOW")
+	def test_classify_temperaturee_breach_passive_cooling(self):
+		self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 10) == 'NORMAL')
+
+	def test_classify_temperaturee_breach_med_active_cooling(self):
+		self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 10) == 'NORMAL')
+
+	def test_classify_temperaturee_breach_hi_active_cooling(self):
+		self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 50) == 'TOO_HIGH')
+
+	def test_function_calls(self):
+		typewise_alert.send_to_controller("TOO_HIGH")
+		typewise_alert.send_to_email("TOO_LOW")
 
 if __name__ == '__main__':
-  unittest.main()
+	unittest.main()
