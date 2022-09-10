6acf6116b8b4d954ea506a89a742378df28ab18f
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-10 23:14:23 +0530

    Completed credit card validator

--- credit_card_validator.py
+++ credit_card_validator.py
@@ -1,5 +1,26 @@
 def validate(card_number):
+	card_number_length = len(card_number) - 1
+
 	if card_number == "":
 		return False
-	elif len(card_number) != 16:
+	elif card_number_length != 15:
 		return False
+
+	numbers_list = [int(ch) for ch in card_number]
+	check_digit = numbers_list.pop()
+	result = 0
+	
+	for (index, number) in enumerate(numbers_list):
+		if ((card_number_length - index) % 2 == 0):
+			multiplier = 1
+		else:
+			multiplier = 2
+		
+		new_number = int(number) * multiplier
+		if new_number > 9:
+			new_number = (new_number % 10) + (new_number // 10)
+
+		result += new_number
+	
+	computed_check_digit = 10 - (result % 10)
+	return check_digit == computed_check_digit
--- credit_card_validator.test.py
+++ credit_card_validator.test.py
@@ -11,5 +11,11 @@ class CreditCardValidator(unittest.TestCase):
 	def test_credit_card_more_than_16(self):
 		self.assertFalse(credit_card_validator.validate("123456789012345678"))
 
+	def test_credit_card_valid_value(self):
+		self.assertTrue(credit_card_validator.validate("4012888888881881"))
+
+	def test_credit_card_invalid_value(self):
+		self.assertFalse(credit_card_validator.validate("4012888888881889"))
+
 if __name__ == "__main__":
 	unittest.main()
