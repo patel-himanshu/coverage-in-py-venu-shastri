66d42cf35a5f83a084c13cbe0d2ce62897c0e519
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-11 19:12:57 +0530

    Add 3 tests with mock for print function

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,5 +1,6 @@
 import unittest
 import typewise_alert
+from unittest.mock import patch, call
 
 
 class TypewiseTest(unittest.TestCase):
@@ -21,9 +22,20 @@ class TypewiseTest(unittest.TestCase):
 	def test_classify_temperaturee_breach_med_active_cooling(self):
 		self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', -10) == 'TOO_LOW')
 
-	def test_function_calls(self):
-		typewise_alert.send_to_controller("TOO_HIGH")
-		typewise_alert.send_to_email("TOO_LOW")
+	@patch('builtins.print')
+	def test_send_to_controller(self, mock_print):
+		typewise_alert.send_to_controller('TOO_LOW')
+		mock_print.assert_called_with(f'{0xfeed}, TOO_LOW')
+
+	@patch('builtins.print')
+	def test_send_to_email_too_low(self, mock_print):
+		typewise_alert.send_to_email('TOO_LOW')
+		assert mock_print.mock_calls == [call('To: a.b@c.com'), call('Hello, the temperature is too low')]
+
+	@patch('builtins.print')
+	def test_send_to_email_too_high(self, mock_print):
+		typewise_alert.send_to_email('TOO_HIGH')
+		assert mock_print.mock_calls == [call('To: a.b@c.com'), call('Hello, the temperature is too high')]
 
 if __name__ == '__main__':
 	unittest.main()
