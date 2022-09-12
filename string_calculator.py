3741b1af13b983f4e03046c76f50dcd9cc66cf51
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-12 14:17:32 +0530

    Improve CCN for string_calculator

--- string_calculator.py
+++ string_calculator.py
@@ -1,10 +1,13 @@
+def add_more_than_one_argument(numbers_list):
+	if len(numbers_list) == 1:
+		return int(numbers_list[0])
+	elif len(numbers_list) == 2:
+		a, b = int(numbers_list[0]), int(numbers_list[1])
+		return a + b
+
 def add(input_str):
     if input_str == "":
         return 0
 
     numbers_list = input_str.split(",")
-    if len(numbers_list) == 1:
-        return int(numbers_list[0])
-    elif len(numbers_list) == 2:
-        a, b = int(numbers_list[0]), int(numbers_list[1])
-        return a + b
+    return add_more_than_one_argument(numbers_list)
